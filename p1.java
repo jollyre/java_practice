bdfb856efb92dc106941be46a5ee241feb063075
jollyre <vaityhardik783@gmail.com>
2025-05-23 11:22:42 +0530

    For Each Practice

---
+++ p1.java
@@ -0,0 +1,16 @@
+class p1{
+public static void main(String[] args) {
+    
+ int arr[]={12,3,44,56,33};
+
+ for(int i:arr){
+    System.out.println(i);
+ }
+ System.out.println("*************************");
+ String arr1[] = {"Hardik","Vivek","Rahul"};
+
+ for(String j:arr1){
+    System.out.println(j);
+ }
+}    
+}
