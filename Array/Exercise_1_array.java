de6839e897b542be887d4a9e18cbf07c111c2ed2
jollyre <vaityhardik783@gmail.com>
2025-06-21 19:31:39 +0530

    uploaded at 21-06-2025 7:31 PM

---
+++ Array/Exercise_1_array.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Exercise_1_array {
+
+    public static void main(String[] args) {
+        int marks[] = new int[3];
+        marks[0] =12;
+        marks[1]= 98;
+        marks[2]= 88;
+
+        for(int i=0; i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
