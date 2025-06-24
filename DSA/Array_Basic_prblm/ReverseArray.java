30fad84ba53b693cae3170bafa3a6621286869b1
jollyre <vaityhardik783@gmail.com>
2025-06-24 19:07:17 +0530

    uploaded at 24-06-2025

---
+++ DSA/Array_Basic_prblm/ReverseArray.java
@@ -0,0 +1,19 @@
+package DSA.Array_Basic_prblm;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int []arr = {1,2,3,4,5};
+        int start =0;
+        int  end = arr.length -1; //4
+        while(start< end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        for(int num: arr){
+            System.out.println(num + " ");
+        }
+    }
+}
---
+++ Inheritance/Single_Level_Inheritance/A.java
@@ -0,0 +1,7 @@
+package Inheritance.Single_Level_Inheritance;
+
+public class A {
+    void m1(){
+        
+    }
+}
