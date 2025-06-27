63035b62e61e70610a83bdced55b9b8e4ea916cc
jollyre <vaityhardik783@gmail.com>
2025-06-27 11:26:43 +0530

    Vertoz technical interview

---
+++ DSA/technicalQuestion/addNumberwithoutThirdVar.java
@@ -0,0 +1,14 @@
+public class addNumberwithoutThirdVar {
+    public static void main(String[] args) {
+        int a = 10; // a = 20 
+        int b = 20; // b = 10
+        
+        a = a +b ; //30
+        b = a -b; //10
+        a = a- b; //20;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
