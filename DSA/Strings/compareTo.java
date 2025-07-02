c94d22b3f6e6ae98192a67758a8fc07ac8ab8eff
jollyre <vaityhardik783@gmail.com>
2025-07-02 20:00:59 +0530

    String Handling

--- DSA/Strings/compareTo.java
+++ DSA/Strings/compareTo.java
@@ -5,6 +5,10 @@ public class compareTo {
         String name1 = "Tony";
     String name2 = "Stark";
 
+    //1 s1 > s2 : +value
+    //2 s1 == s2: 0
+    //3 s1<s2 : -ve value
+
     if(name1.compareTo(name2) == 0){
         System.out.println("String are equal");
     }else{
