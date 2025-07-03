2c42edb4e4e5e770a203bfdf895da2918be96958
jollyre <vaityhardik783@gmail.com>
2025-07-03 18:30:16 +0530

    String Handling

---
+++ DSA/Array_Basic_prblm/substring.java
@@ -0,0 +1,10 @@
+package DSA.Array_Basic_prblm;
+
+public class substring {
+    public static void main(String[] args) {
+        String sentence = "Java is Fun";
+        
+        String s1 = sentence.substring(0,5); //5 mean it will select index 4 
+        System.out.println(s1);
+    }
+}
